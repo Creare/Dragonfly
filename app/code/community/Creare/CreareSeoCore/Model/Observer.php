1cfc63e45501f3b5e1dc4d3dabd0ab55b9cd24f6
Adam Moss <adammoss7@gmail.com>
2013-11-14 11:07:32 +0000

    Update

--- app/code/community/Creare/CreareSeoCore/Model/Observer.php
+++ app/code/community/Creare/CreareSeoCore/Model/Observer.php
@@ -37,6 +37,7 @@ class Creare_CreareSeoCore_Model_Observer extends Mage_Core_Model_Abstract
         /* Modify XML Sitemap data before save */
         public function applySitemapChanges($observer)
         {
+            echo "TEST";
             Mage::log('My log entry', null, 'xmlsitemap.log');
         }
 
