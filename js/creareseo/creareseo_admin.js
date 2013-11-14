3acc6954cd69a155a57094e46fe4a298788fd974
Robert Kent <robert@creare.co.uk>
2013-11-14 11:56:47 +0000

    Removed comments from seo settings section

--- js/creareseo/creareseo_admin.js
+++ js/creareseo/creareseo_admin.js
@@ -1,5 +1,13 @@
 document.observe("dom:loaded", function() {
 	$('creareseo-check').observe('click', function(event) {
-		alert('test');
+		new Ajax.Request(BASE_URL+'creareseo/test/', {
+		  method:'get',
+		  onSuccess: function(transport) {
+		    var response = transport.responseText || "no response text";
+		    $('creareseo-check-results').insert(response);
+		  },
+		  onFailure: function() { alert('Something went wrong...'); }
+		});
+		
 	});
 });
