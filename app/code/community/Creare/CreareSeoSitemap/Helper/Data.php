3c31fc050bac83b9b07e087f8e96fe128f344fc0
Robert Kent <robert@creare.co.uk>
2013-11-14 10:59:15 +0000

    Updates

--- app/code/community/Creare/CreareSeoSitemap/Helper/Data.php
+++ app/code/community/Creare/CreareSeoSitemap/Helper/Data.php
@@ -4,26 +4,26 @@ class Creare_CreareSeoSitemap_Helper_Data extends Mage_Core_Helper_Abstract
 {
 	public function showCMS()
 	{
-		return Mage::getStoreConfig('creareseo/creareseositemap/showcms');
+		return Mage::getStoreConfig('creareseositemap/sitemap/showcms');
 	}
 
 	public function showCategories()
 	{
-		return Mage::getStoreConfig('creareseo/creareseositemap/showcategories');
+		return Mage::getStoreConfig('creareseositemap/sitemap/showcategories');
 	}
 
 	public function showXMLSitemap()
 	{
-		return Mage::getStoreConfig('creareseo/creareseositemap/showxml');
+		return Mage::getStoreConfig('creareseositemap/sitemap/showxml');
 	}
 
 	public function showAccount()
 	{
-		return Mage::getStoreConfig('creareseo/creareseositemap/showaccount');
+		return Mage::getStoreConfig('creareseositemap/sitemap/showaccount');
 	}
 
 	public function showContact()
 	{
-		return Mage::getStoreConfig('creareseo/creareseositemap/showcontact');
+		return Mage::getStoreConfig('creareseositemap/sitemap/showcontact');
 	}
 }
