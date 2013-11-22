b065a386dc715821ad49d37117953c948fa8810a
Robert Kent <robert@creare.co.uk>
2013-11-22 14:38:36 +0000

    updating data.php

--- app/code/community/Creare/CreareSeoCore/Helper/Data.php
+++ app/code/community/Creare/CreareSeoCore/Helper/Data.php
@@ -1,5 +1,86 @@
 <?php
 class Creare_CreareSeoCore_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public function getConfigPath($observer)
+    {
+        return Mage::app()->getRequest()->getControllerName().'_'.Mage::app()->getRequest()->getParam('section');
+    }
+    
+    
+    /* 
+     * Takes the file and the configuration field and saves the
+     * current file data to the database before the field is loaded
+     */
+    
+    public function saveFileContentToConfig($file, $field)
+    {
+        $io = new Varien_Io_File();
+        $io->open(array('path' => Mage::getBaseDir()));
+        
+        if ($io->fileExists($file))
+        {
+            try
+            {
+                $contents = $io->read($file);
+                Mage::getModel('core/config')->saveConfig('creareseocore/files/'.$field, $contents);
+                
+            } catch(Mage_Core_Exception $e)
+            {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+            
+        $io->streamClose();
+    }
+    
+    /* 
+     * Takes the file, post data and the configuration field and 
+     * writes the post data to the file.
+     */
+    
+    public function writeFile($file, $post, $field)
+    {
+        if (Mage::getStoreConfig('creareseocore/files/'.$field) == $post)
+        {
+            echo "POST:";
+            var_dump($post);
+            echo "<br />";
+            echo "<br />";
+            echo "In DB:";
+            echo "<br />";
+            echo "<br />";
+            var_dump(Mage::getStoreConfig('creareseocore/files/'.$field));
+            
+            die($field." the same");
+            return false;
+        }
+        
+        $io = new Varien_Io_File();
+        $io->open(array('path' => Mage::getBaseDir()));
+        
+        if ($io->fileExists($file) && $io->isWriteable($file))
+        {
+            try
+            {
+                $io->streamOpen($file);
+                $io->streamWrite($post);
+                
+            } catch(Mage_Core_Exception $e)
+            {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+            
+        $io->streamClose();
+    }
+    
+    public function robotstxt()
+    {
+        return 'robots.txt';
+    }
+    
+    public function htaccess()
+    {
+        return '.htaccess';
+    }
 }
