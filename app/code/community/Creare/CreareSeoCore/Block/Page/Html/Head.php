d708dc4ed243dd19ce8082b8b1b000f1cd35fce7
Adam Moss <adammoss7@gmail.com>
2013-12-11 09:13:37 +0000

    no message

--- app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head.php
+++ app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head.php
@@ -9,7 +9,6 @@ class Creare_CreareSeoCore_Block_Page_Html_Head extends Mage_Page_Block_Html_Hea
             {
                 $this->_data['title'] = $this->metaHelper()->getDefaultTitle();
             } else {
-            }
                 $this->_data['title'] = $this->getDefaultTitle();
             }
         }
