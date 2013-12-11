3e9e1870d58898c976cb22eb5d2f89d5163e981d
Adam Moss <adammoss7@gmail.com>
2013-12-11 11:40:08 +0000

    big push for meta title & description functionality

--- app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head.php
+++ app/code/community/Creare/CreareSeoCore/Block/Page/Html/Head.php
@@ -4,22 +4,54 @@ class Creare_CreareSeoCore_Block_Page_Html_Head extends Mage_Page_Block_Html_Hea
 {
     public function getTitle()
     {
-        if (empty($this->_data['title'])) {
-            if ($this->metaHelper()->getDefaultTitle())
+        $pagetype = $this->metaHelper()->getPageType();
+        
+        if ($pagetype && $pagetype != 'cms_page')
+        {
+            if (!$pagetype->model->getMetaTitle())
             {
-                $this->_data['title'] = $this->metaHelper()->getDefaultTitle();
-            } else {
-                $this->_data['title'] = $this->getDefaultTitle();
+                $this->setConfigTitle($pagetype->code);
             }
         }
+        
+        if (empty($this->_data['title'])) {
+            $this->_data['title'] = $this->getDefaultTitle();
+        }
+        
         return htmlspecialchars(html_entity_decode(trim($this->_data['title']), ENT_QUOTES, 'UTF-8'));
+    }
+    
+    public function setConfigTitle($pagetype)
+    {
+        if ($this->metaHelper()->config($pagetype.'_title_enabled'))
+        {
+            $this->_data['title'] = $this->metaHelper()->getDefaultTitle($pagetype);
+        }
+    }
+    
+    public function setConfigMetaDescription($pagetype)
+    {
+        if ($this->metaHelper()->config($pagetype.'_metadesc_enabled'))
+        {
+            $this->_data['description'] = $this->metaHelper()->getDefaultMetaDescription($pagetype);
+        }
     }
     
     
     public function getDescription()
     {
+        $pagetype = $this->metaHelper()->getPageType();
+        
+        if ($pagetype)
+        {
+            if (!$pagetype->model->getMetaDescription())
+            {
+                $this->setConfigMetaDescription($pagetype->code);
+            }
+        }
+        
         if (empty($this->_data['description'])) {
-            $this->_data['description'] = Mage::getStoreConfig('design/head/default_description');
+            $this->_data['description'] = "";
         }
         return $this->_data['description'];
     }
--- app/code/community/Creare/CreareSeoCore/Helper/Meta.php
+++ app/code/community/Creare/CreareSeoCore/Helper/Meta.php
@@ -1,39 +1,59 @@
 <?php
 class Creare_CreareSeoCore_Helper_Meta extends Mage_Core_Helper_Abstract
 {
-    public function getDefaultTitle()
+    public function getDefaultTitle($pagetype)
     {
-        if ($this->getPageType())
-        {
-            $title = $this->config($this->getPageType().'_title');
-            return $this->shortcodeString($title);
-        }
+        $title = $this->config($pagetype.'_title');
+        return $this->shortcode($title);
+    }
+    
+    public function getDefaultMetaDescription($pagetype)
+    {
+        $metadesc = $this->config($pagetype.'_metadesc');
+        return $this->shortcode($metadesc);
     }
     
     public function getPageType()
     {
+        $registry = new Varien_Object;
+        
         if (Mage::registry('current_product'))
         {
-            return 'product';
+            $registry->code = 'product';
+            $registry->model = Mage::registry('current_product');
+            
+            return $registry;
+            
         } elseif (Mage::registry('current_category'))
         {
-            return 'category';
+            $registry->code = 'category';
+            $registry->model = Mage::registry('current_category');
+            
+            return $registry;
+            
         } elseif (Mage::registry('current_cms_page'))
         {
-            return 'cms_page';
+            $registry->code = 'cms_page';
+            $registry->model = Mage::registry('current_cms_page');
+            
+            return $registry;
+            
         } else {
             return false;
+            
         }
     }
     
-    private function config($path)
+    public function config($path)
     {
         return Mage::getStoreConfig('creareseocore/metadata/'.$path);
     }
     
-     function shortcodeString($string)
-     {
-            preg_match_all("/\[(.*?)\]/", $string, $matches);
+    public function shortcode($string)
+    {
+        $pagetype = $this->getPageType();
+        
+        preg_match_all("/\[(.*?)\]/", $string, $matches);
 
             for($i = 0; $i < count($matches[1]); $i++)
             {
@@ -44,7 +64,7 @@ class Creare_CreareSeoCore_Helper_Meta extends Mage_Core_Helper_Abstract
                     $string = str_replace($matches[0][$i], Mage::app()->getStore()->getName(), $string);
                 } else {
                 
-                switch ($this->getPageType())
+                switch ($pagetype->code)
                 {
                     case 'product' :
                         $attribute = $this->productAttribute(Mage::registry('current_product'), $tag);
@@ -62,7 +82,7 @@ class Creare_CreareSeoCore_Helper_Meta extends Mage_Core_Helper_Abstract
             return $string;
      }
      
-     function productAttribute($product, $attribute)
+     public function productAttribute($product, $attribute)
      {
          if ($product->getData($attribute))
          {
@@ -71,14 +91,11 @@ class Creare_CreareSeoCore_Helper_Meta extends Mage_Core_Helper_Abstract
 
      }
      
-     function attribute($model, $attribute)
+     public function attribute($model, $attribute)
      {
          if ($model->getData($attribute))
          {
-             
-             
-            return $product->getResource()->getAttribute($attribute)->getFrontend()->getValue($product);
+            return $model->getData($attribute);
          }
-
      }
 }
